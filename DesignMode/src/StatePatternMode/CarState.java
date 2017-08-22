84a94bbf0df3fce012e832f92ffc1106e21b16ce
xuefu_vegetate <252295126@qq.com>
2017-08-22 16:25:45 +0800

    增加状态模式设计

---
+++ DesignMode/src/StatePatternMode/CarState.java
@@ -0,0 +1,11 @@
+package StatePatternMode;
+
+/**
+ * Created by lx on 2017/8/22.
+ */
+public interface CarState {
+    public void upSpeed();//加速
+    public void downSpeed();//减速
+    public void turnOnLight();//开灯
+    public void turnDownLight();//关灯
+}
