000b46f86d47ea3c6fca7210a0cf0cf829bc0d90
xuefu_vegetate <252295126@qq.com>
2018-11-08 15:05:32 +0800

    新增桥接模式和建造者模式

---
+++ DesignMode/src/BuilderPattern/BuilderTest.java
@@ -0,0 +1,17 @@
+package BuilderPattern;
+
+/**
+ * Created by lx on 2018/11/8.
+ */
+
+public class BuilderTest {
+    public static void main(String[] args) {
+        LiveDirector liveDirector = new LiveDirector();
+
+        //жысю
+        PersonLive zhongyong = liveDirector.generateLive(new GiftedChildrenBuilder());
+
+        //╫╙всяю
+        PersonLive jiangziya = liveDirector.generateLive(new OldSuccessBuilder());
+    }
+}
---
+++ DesignMode/src/BuilderPattern/LiveBuilder.java
@@ -0,0 +1,15 @@
+package BuilderPattern;
+
+/**
+ * Created by lx on 2018/11/8.
+ */
+public interface LiveBuilder {
+    public void buildBaby();
+    public void buildChildren();
+    public void buildYouth();
+    public void buildMidLife();
+    public void buildOld();
+
+    public PersonLive getPerson();
+
+}
